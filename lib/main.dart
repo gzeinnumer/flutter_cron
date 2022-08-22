cc3e40a621eb99e1ad58ddc11b14a68e6689f51d
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-08-23 01:14:35 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+
+import 'package:cron/cron.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Timer timer;
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    final cron = Cron();
+    print('fun start');
+
+    // cron.schedule(Schedule.parse('* * * * *'), () async {
+    //   print('execute every minute');
+    //   _incrementCounter();
+    // });
+    cron.schedule(Schedule.parse('00 00 * * *'), () async {
+      print('execute every minute');
+      _incrementCounter();
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Builder(
+        builder: (context) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
